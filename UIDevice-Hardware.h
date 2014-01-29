8a36584ac74131bf9e75bfaad0c2236ed95fd346
Nitesh Kumar Pal <nitesh.pal@weboniselab.com>
2014-01-29 16:25:21 +0530

    UIdeviceExtension for ios app developers

---
+++ UIDevice-Hardware.h
@@ -0,0 +1,112 @@
+/*
+ Erica Sadun, http://ericasadun.com
+ iPhone Developer's Cookbook, 6.x Edition
+ BSD License, Use at your own risk
+ */
+
+#import <UIKit/UIKit.h>
+
+#define IFPGA_NAMESTRING                @"iFPGA"
+
+#define IPHONE_1G_NAMESTRING            @"iPhone 1G"
+#define IPHONE_3G_NAMESTRING            @"iPhone 3G"
+#define IPHONE_3GS_NAMESTRING           @"iPhone 3GS" 
+#define IPHONE_4_NAMESTRING             @"iPhone 4" 
+#define IPHONE_4S_NAMESTRING            @"iPhone 4S"
+#define IPHONE_5_NAMESTRING             @"iPhone 5"
+#define IPHONE_5c_NAMESTRING            @"iPhone 5c"
+#define IPHONE_5s_NAMESTRING            @"iPhone 5s"
+#define IPHONE_UNKNOWN_NAMESTRING       @"Unknown iPhone"
+
+#define IPOD_1G_NAMESTRING              @"iPod touch 1G"
+#define IPOD_2G_NAMESTRING              @"iPod touch 2G"
+#define IPOD_3G_NAMESTRING              @"iPod touch 3G"
+#define IPOD_4G_NAMESTRING              @"iPod touch 4G"
+#define IPOD_UNKNOWN_NAMESTRING         @"Unknown iPod"
+
+#define IPAD_1G_NAMESTRING              @"iPad 1G"
+#define IPAD_2G_NAMESTRING              @"iPad 2G"
+#define IPAD_3G_NAMESTRING              @"iPad 3G"
+#define IPAD_4G_NAMESTRING              @"iPad 4G"
+#define IPAD_UNKNOWN_NAMESTRING         @"Unknown iPad"
+
+#define APPLETV_2G_NAMESTRING           @"Apple TV 2G"
+#define APPLETV_3G_NAMESTRING           @"Apple TV 3G"
+#define APPLETV_4G_NAMESTRING           @"Apple TV 4G"
+#define APPLETV_UNKNOWN_NAMESTRING      @"Unknown Apple TV"
+
+#define IOS_FAMILY_UNKNOWN_DEVICE       @"Unknown iOS device"
+
+#define SIMULATOR_NAMESTRING            @"iPhone Simulator"
+#define SIMULATOR_IPHONE_NAMESTRING     @"iPhone Simulator"
+#define SIMULATOR_IPAD_NAMESTRING       @"iPad Simulator"
+#define SIMULATOR_APPLETV_NAMESTRING    @"Apple TV Simulator" // :)
+
+typedef enum {
+    UIDeviceUnknown, //0
+    
+    UIDeviceSimulator, //1
+    UIDeviceSimulatoriPhone,
+    UIDeviceSimulatoriPad,
+    UIDeviceSimulatorAppleTV, //4
+    
+    UIDevice1GiPhone, //5
+    UIDevice3GiPhone,
+    UIDevice3GSiPhone, //7
+    UIDevice4iPhone, //8
+    UIDevice4SiPhone,//9
+    UIDevice5iPhone, //10
+    UIDevice5ciPhone, //11
+    UIDevice5siPhone, //12
+    
+    UIDevice1GiPod, //13
+    UIDevice2GiPod,
+    UIDevice3GiPod,
+    UIDevice4GiPod, //16
+    
+    UIDevice1GiPad, //17
+    UIDevice2GiPad,
+    UIDevice3GiPad,
+    UIDevice4GiPad, //20
+    
+    UIDeviceAppleTV2, //21
+    UIDeviceAppleTV3,
+    UIDeviceAppleTV4, //23
+    
+    UIDeviceUnknowniPhone, //24
+    UIDeviceUnknowniPod,   //25
+    UIDeviceUnknowniPad,   //26
+    UIDeviceUnknownAppleTV,//27
+    UIDeviceIFPGA,         //28
+
+} UIDevicePlatform;
+
+typedef enum {
+    UIDeviceFamilyiPhone,
+    UIDeviceFamilyiPod,
+    UIDeviceFamilyiPad,
+    UIDeviceFamilyAppleTV,
+    UIDeviceFamilyUnknown,
+    
+} UIDeviceFamily;
+
+@interface UIDevice (Hardware)
+- (NSString *) platform;
+- (NSString *) hwmodel;
+- (NSUInteger) platformType;
+- (NSString *) platformString;
+
+- (NSUInteger) cpuFrequency;
+- (NSUInteger) busFrequency;
+- (NSUInteger) cpuCount;
+- (NSUInteger) totalMemory;
+- (NSUInteger) userMemory;
+
+- (NSNumber *) totalDiskSpace;
+- (NSNumber *) freeDiskSpace;
+
+- (NSString *) macaddress;
+
+- (BOOL) hasRetinaDisplay;
+- (UIDeviceFamily) deviceFamily;
+@end
